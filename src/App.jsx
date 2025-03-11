d4e2b620844afde90c78d1f497513702f9e9e56e
iyrd <chilromko@gmail.com>
2025-03-11 20:09:06 +0100

    333

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import { useState, useEffect } from 'react';
 import { nanoid } from 'nanoid';
-import ContactForm from '../ContactForm/ContactForm';
-import SearchBox from '../SearchBox/SearchBox';
-import ContactList from '../ContactList/ContactList';
+import ContactForm from './components/ContactForm/ContactForm';
+import SearchBox from './components/SearchBox/SearchBox';
+import ContactList from './components/ContactList/ContactList';
 import styles from './App.module.css';
 
 
